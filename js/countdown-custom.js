a1c67876f0973991b6001504ec69a5345c815913
AselaPathirage <aselapathirage@gmail.com>
2021-06-02 20:25:54 +0530

    RevolUX landing 2021

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+    $(function () {
+        $('#defaultCountdown').countdown({ until: new Date(2021, 05, 12, 24) }); // year, month, date, hour
+    });
+});
+
